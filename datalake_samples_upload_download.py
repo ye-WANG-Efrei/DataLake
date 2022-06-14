42de8bf78af595701ba68a631345ec8619536e4d
Wang ye <ye.wang.20182131@efrei.net>
2022-06-14 12:10:42 +0200

    Update datalake_samples_upload_download.py
    update: use DataLakeServiceClient instead to file

--- datalake_samples_upload_download.py
+++ datalake_samples_upload_download.py
@@ -11,10 +11,11 @@ FILE: datalake_samples_upload_download.py
 DESCRIPTION:
     This sample demonstrates:
     * Set up a file system
+    * Create file
     * Append data to the file
     * Flush data to the file
     * Get file properties
-    * the uploaded data
+    * Download the uploaded data
     * Delete file system
 USAGE:
     python datalake_samples_upload_download.py
@@ -25,6 +26,8 @@ USAGE:
 
 import os
 import random
+
+
 list_files=[]
 count=0
 def list_dir(file_dir,list_files,count):
@@ -45,10 +48,81 @@ def list_dir(file_dir,list_files,count):
 
     return list_files,count
 
-list_csv,count = list_dir('./',list_files,count)
+list_files,count = list_dir('./',list_files,count)
 
 
-from azure.storage.filedatalake import DataLakeServiceClient,
+from azure.storage.filedatalake import (
+    DataLakeServiceClient,
+)
+
+
+
+def upload_download_sample(filesystem_client,SOURCE_FILE):
+    # create a file before writing content to it
+    file_name = "testfile"
+    print("Creating a file named '{}'.".format(file_name))
+    # [START create_file]
+    file_client = filesystem_client.get_file_client(file_name)
+    file_client.create_file()
+    # [END create_file]
+
+    # prepare the file content with 4KB of random data
+    with open(SOURCE_FILE, "rb") as data:
+        file_client.append_data(data,offset=0)
+        #file_client.flush_data(len(data))
+    #
+    #     # append data to the file
+    #     # the data remain uncommitted until flush is performed
+    #     print("Uploading data to '{}'.".format(file_name))
+    #     file_client.append_data(data=file_content[0:1024], offset=0, length=1024)
+    #     file_client.append_data(data=file_content[1024:2048], offset=1024, length=1024)
+    #     # [START append_data]
+    #     file_client.append_data(data=file_content[2048:3072], offset=2048, length=1024)
+    #     # [END append_data]
+    #     file_client.append_data(data=file_content[3072:4096], offset=3072, length=1024)
+
+    # data is only committed when flush is called
+
+
+    # Get file properties
+    # [START get_file_properties]
+    properties = file_client.get_file_properties()
+    # [END get_file_properties]
+
+    # read the data back
+    print("Downloading data from '{}'.".format(file_name))
+    # [START read_file]
+    download = file_client.download_file()
+    downloaded_bytes = download.readall()
+    # [END read_file]
+
+    # verify the downloaded content
+#     if file_content == downloaded_bytes:
+#         print("The downloaded data is equal to the data uploaded.")
+#     else:
+#         print("Something went wrong.")
+
+    # Rename the file
+    # [START rename_file]
+    new_client = file_client.rename_file(file_client.file_system_name + '/' + 'newname')
+    # [END rename_file]
+
+    # download the renamed file in to local file
+    with open(SOURCE_FILE, 'wb') as stream:
+        download = new_client.download_file()
+        download.readinto(stream)
+
+    # [START delete_file]
+    new_client.delete_file()
+    # [END delete_file]
+
+# help method to provide random bytes to serve as file content
+# def get_random_bytes(size):
+#     rand = random.Random()
+#     result = bytearray(size)
+#     for i in range(size):
+#         result[i] = int(rand.random()*255)  # random() is consistent between python 2 and 3
+#     return bytes(result)
 
 
 def run():
@@ -66,14 +140,14 @@ def run():
     print("Generating a test filesystem named '{}'.".format(fs_name))
 
     # create the filesystem
-    service_client.create_file_system()
-    while(count):
-        try:
-            with open(list_csv[count-1], "rb") as data:
-                service_client.upload_blob(data)
-        finally:
-             #clean up the demo filesystem
-             service_client.delete_file_system()
+    filesystem_client = service_client.create_file_system(file_system=fs_name)
+
+    # invoke the sample code
+    while (count):
+        SOURCE_FILE = list_files[count-1]
+        upload_download_sample(filesystem_client,SOURCE_FILE)
+        count-=1
+
 
 
 if __name__ == '__main__':
